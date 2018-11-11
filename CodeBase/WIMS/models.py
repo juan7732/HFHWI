ca2173d5fc506fb773483d010d49bff75708a03b
Juan <juan7732@gmail.com>
2018-11-10 22:48:40 -0600

    Added user model and created superuser

--- CodeBase/WIMS/models.py
+++ CodeBase/WIMS/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class user(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    Phone = models.CharField(max_length=45)
+    ZipCode = models.IntegerField()
+    DateJoined = models.DateTimeField()
+
+    def __str__(self):
+        return self.FirstName + ' ' + self.LastName
